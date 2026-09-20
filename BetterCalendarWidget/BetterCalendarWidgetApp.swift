27787028046392853d5bc9748d32a7dcd32a4afb
paulrobben <github@paulrobben.de>
2026-09-20 12:10:12 +0200

    Initial Commit

---
+++ BetterCalendarWidget/BetterCalendarWidgetApp.swift
@@ -0,0 +1,32 @@
+//
+//  BetterCalendarWidgetApp.swift
+//  BetterCalendarWidget
+//
+//  Created by Paul on 20.09.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BetterCalendarWidgetApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
